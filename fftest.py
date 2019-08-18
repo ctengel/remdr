99c1c84d9874d9425fda65c834e2debdb4b57604
Chris Engelhart <engleman@gmail.com>
2019-08-17 21:03:28 -0400

    Python list all devices

--- fftest.py
+++ fftest.py
@@ -4,5 +4,11 @@ import sys
 import evdev
 import pprint
 
+print('All devices:')
+for name in evdev.list_devices():
+    print(name)
+print()
+
+print('This device')
 dev1 = evdev.device.InputDevice(sys.argv[1])
 pprint.pprint(dev1.capabilities(True, True))
