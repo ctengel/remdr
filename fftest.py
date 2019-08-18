d1c2b4ddafa05926fcbc1b6b785c29f756cdff90
Chris Engelhart <engleman@gmail.com>
2019-08-17 21:27:43 -0400

    More python evdev progress

--- fftest.py
+++ fftest.py
@@ -11,4 +11,31 @@ print()
 
 print('This device')
 dev1 = evdev.device.InputDevice(sys.argv[1])
-pprint.pprint(dev1.capabilities(True, True))
+caps = dev1.capabilities(True, True)
+pprint.pprint(caps)
+
+
+ff_key = None
+ff_modes = []
+for k, v in caps.items():
+    if k[0] == 'EV_FF':
+        ff_key = k[1]
+        ff_modes = v
+        break
+
+print(ff_key)
+pprint.pprint(ff_modes)
+
+rumb_id = None
+gain_id = None
+for i in ff_modes:
+    if i[0] == 'FF_RUMBLE' or 'FF_RUMBLE' in i[0]:
+        rumb_id = i[1]
+    if i[0] == 'FF_GAIN' or 'FF_GAIN' in i[0]:
+        gain_id = i[1]
+
+print('Rumble:', rumb_id)
+print('Gain:', gain_id)
+
+#gain = evdev.ff.Gain()
+rumble = evdev.ff.Rumble()
